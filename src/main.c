dcb8d1875def18ab1cbd62694df014cbcf0bf022
rare-wolf <palmeredo@proton.me>
2025-12-13 13:31:24 +0400

    restarted the written the code of creating/opening a data base file, everything works smoothly now no errors so far, now starting to add the db_header creation/validation functionality

--- src/main.c
+++ src/main.c
@@ -48,25 +48,17 @@ int main (int argc, char *argv[]) {
 	if (newfile) {
 		dbfd = create_db_file(filepath);
 		if (dbfd == STATUS_ERROR) {
-			printf("Unable to create database file\n");
+			printf("unable to create database file\n");
 			return -1;
 		}
 	} else {
 		dbfd = open_db_file(filepath);
 		if (dbfd == STATUS_ERROR) {
-			printf("Unable to open the database file\n");
+			printf("unable to open database file\n");
 			return -1;
 		}
 	}
 
-	// validate the database header 
-	if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
-		printf("Failed to validate database header\n");
-		return -1;
-	}
-
-	// write data to file
-	output_file(dbfd, dbhdr);
 
 
 	return 0;

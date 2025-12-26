4dd2b16bc573bdd7bbacdb159e6f92c90579bac0
rare-wolf <palmeredo@proton.me>
2025-12-26 11:49:05 +0400

    finished the read_employees() fnctionality, now working on output_file() functionality

--- src/parse.c
+++ src/parse.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> // for read
+#include <fcntl.h> // for open
 #include <stdbool.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/stat.h> // for fstat
 #include <stdlib.h>
 
 #include "common.h"
 #include "parse.h"
 
-// the analogy of this program is to read data from (disc) and parse it in (memory)
-// then write data back to (disc)
 
-// function to write the data back to disc 
-void output_file(int fd, struct dbheader_t *dbhdr) {
-    if (fd < 0) {
-        printf("Got a bad FD from the user\n");
-        return;
+
+// function to create the file header
+int create_db_header(struct dbheader_t **headerOut) {
+
+
+    // create a pointer in the (heap) to the header, to be able to pass it 
+    // to other functions in the program
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    // make sure calloc() did not fail :
+    if (header == -1) {
+        printf("Malloc() failed to create db header\n");
+        return  STATUS_ERROR;
     }
+    // create the values of a new header :
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
 
-    // Convert from host byte order (little-endian) 
-    // to network byte order (big-endian),
-    // so it can be correctly stored back in the disc.
-    dbhdr->magic = htonl(dbhdr->magic);
-    dbhdr->filesize = htonl(dbhdr->filesize);
-    dbhdr->count = htonl(dbhdr->count);
-    dbhdr->version = htonl(dbhdr->version);
-
-    // return the cursor that types data to the file to the begining of the file
-    // because when we read the file earlier via read(), the cursor went to the end
-    // of the file 
-    lseek(fd, 0, SEEK_SET);
-    return;
+    // store the pointer of the created header to use it later in the program
+    *headerOut = header;
+    // return if program success:
+    return STATUS_SUCCESS;
 }
 
 // function to validate a file header 
@@ -46,7 +48,7 @@ int validate_db_header (int fd, struct dbheader_t **headerOut) {
     // otherwise, allocate memory space in the heap for the dbheader 
     struct dbheader_t *header = calloc(1,sizeof(struct dbheader_t));
     // when calloc fails, it returns (NULL) not (-1)
-    if (header == NULL) {
+    if (header == NULL ) {
         printf("Malloc failed to create a db header\n");
         return STATUS_ERROR;
     }
@@ -58,8 +60,6 @@ int validate_db_header (int fd, struct dbheader_t **headerOut) {
 	}
 
     // Convert from network byte order (big-endian) 
-    // to host byte order (little-endian),
-    // so it can be correctly processed by the system.
     header->version = ntohs(header->version);
     header->count = ntohs(header->count);
     header->magic = ntohl(header->magic);
@@ -67,17 +67,20 @@ int validate_db_header (int fd, struct dbheader_t **headerOut) {
 
     // after we read teh file in the byte order that our system 
     // can understand, we can run our checks on the file header
-    if (header->magic != 1) {
+
+    // check magic value of our file
+    if (header->magic != HEADER_MAGIC) {
         printf("Improper header magic\n");
-        return -1;
+        return STATUS_ERROR;
     }
 
-        if (header->version != 1) {
+    // check version of our file
+    if (header->version != 1) {
         printf("Improper header version\n");
-        return -1;
+        return STATUS_ERROR;
     }
 
-    // ?
+    // check the size of our file 
     struct stat dbstat = {0};
     // fstat() is a system call used to:
     // 1. Get the file status information for the file identified by the file descriptor 'fd'.
@@ -88,32 +91,67 @@ int validate_db_header (int fd, struct dbheader_t **headerOut) {
     // If they don't match, print an error message indicating the file is corrupted.
     if (header->filesize != dbstat.st_size) {
         printf("Corrupted database\n");
-        return -1; // return -1 if file corrupted
-    }
-}
-
-// function to create the file header
-int create_db_header(int fd, struct dbheader_t **headerOut) {
-
-    // create a pointer in the (heap) to the header, to be able to pass it 
-    // to other functions in the program
-    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    // make sure calloc() did not fail :
-    if (header == NULL) {
-        printf("Malloc() failed to create db header\n");
-        return  STATUS_ERROR;
+        return STATUS_ERROR; // return -1 if file corrupted
     }
 
-    // create the values of a new header :
-    header->version = 0x1;
-    header->count = 0;
-    header->magic = HEADER_MAGIC;
-    header->filesize = sizeof(struct dbheader_t);
-
-    // store the pointer of the created header to use it later in the program
+    // pass the header out 
     *headerOut = header;
 
-    // return if program success:
+    // if everything goes well, return success : 
     return STATUS_SUCCESS;
 }
-int validate_db_header(int fd, struct dbheader_t **headerOut);
+
+int read_employees(int fd, struct dbheader_t *header, struct employee_t **employeesOut) {
+    
+    // if got a bad fd from user, return an error    
+    if (fd < 0) {
+        printf("Got a bad FD from the user\n");
+        return STATUS_ERROR;
+    }
+
+    //figure out how many employees 
+    int employeesNum = header -> count;
+
+    //allocate memory for struct *employeesOut
+    struct employee_t *employees = calloc (employeesNum, sizeof(struct employee_t));
+    if (employees == NULL) {
+        printf("Failed to allocate memory \n");
+        return STATUS_ERROR;
+    }
+
+    // read data from the file 
+    employees = read(fd, &employees , sizeof(struct employee_t) * employeesNum); 
+
+    // store the data read in the pointer
+    *employeesOut  = employees;
+
+    return STATUS_SUCCESS;
+}
+
+// function to write the data back to disc 
+void output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees){
+    if (fd < 0) {
+        printf("Got a bad FD from the user\n");
+        return;
+    }
+
+    // Convert from host byte order (little-endian) 
+    // to network byte order (big-endian),
+    // so it can be correctly stored back in the disc.
+    dbhdr->count = htons(dbhdr->count);
+    dbhdr->version = htons(dbhdr->version);
+    dbhdr->magic = htonl(dbhdr->magic);
+    dbhdr->filesize = htonl(dbhdr->filesize);
+
+    // return the cursor that types data to the file to the begining of the file
+    // because when we read the file earlier via read(), the cursor went to the end
+    // of the file 
+    lseek(fd, 0, SEEK_SET);
+
+    //write data to the file 
+    write (fd, dbhdr, sizeof(struct dbheader_t));
+    
+    return;
+}
+
+
